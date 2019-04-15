3b7d428ab6d7cf9b650f2c6a5d5c2066740c7713
amel_ghassen <amel.harrath.92@gmail.com>
2019-04-15 23:46:53 +0100

    Deplyed the model to the azure ML service

---
+++ score.py
@@ -0,0 +1,21 @@
+import json
+import numpy as np
+import os
+import pickle
+from sklearn.externals import joblib
+from sklearn.linear_model import LogisticRegression
+from azureml.core.model import Model
+
+# load the model
+def init():
+    global model
+    # retrieve the path to the model file using the model name
+    model_path = Model.get_model_path('model')
+    model = joblib.load(model_path)
+
+# Passes data to the model and returns the prediction
+def run(raw_data):
+    data = np.array(json.loads(raw_data)['data'])
+    # make prediction
+    y_hat = model.predict(data)
+    return json.dumps(y_hat.tolist())
